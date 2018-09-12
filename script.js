47973272c3b1aaa61102a7763d7f5b8b5b74032e
byrammr@gmail.com <Bayram@osxs-MacBook-Air.local>
2018-09-12 19:43:22 -0400

    New Version to do list

--- script.js
+++ script.js
@@ -1,71 +1,64 @@
 window.onload=function(){
+//  Creating close button "x" and appending to each list item
 var liList= document.getElementsByTagName('LI');
-for (var i =0; i <= liList.length-1; i++) {
-var span = document.createElement("span");
-    var xSpan = document.createTextNode("x");
-    span.appendChild(xSpan);
-    span.className ='close';
-    liList[i].appendChild(span);
+    for (var i =0; i <= liList.length-1; i++) {
+        var span = document.createElement("span");
+        var xSpan = document.createTextNode("x");
+        span.appendChild(xSpan);
+        span.className ='close';
+        liList[i].appendChild(span);
+    }
+//code for to display checked list item 
+var ulList = document.getElementsByTagName('UL');
+    for (var i=0; i<=ulList.length-1; i++){
+        var list =  ulList[i];
+        list.addEventListener('click', function(ev){
+            if (ev.target.tagName === 'LI') {  
+            ev.target.classList.toggle("checked");
+            }
+        });
+    }
 
-}
-
-    var list =  document.querySelector('ul');
- list.addEventListener('click', function(ev){
-
-  if (ev.target.tagName === 'LI') {  
-ev.target.classList.toggle("checked");}
- 
-});
-
-//window.onload=function(){
-    var addButton = document.querySelector(".addBtn");
-    addButton.addEventListener('click',function() {
-    var txt = document.getElementById("myInput").value;
-    var node = document.createElement("LI");
-    var txtNode = document.createTextNode(txt);
-    node.appendChild(txtNode);
-    document.getElementById("myUL").appendChild(node);
-    document.getElementById('myInput').value =''; 
-
-    var span = document.createElement("span");
-    var xSpan = document.createTextNode("x");
-    span.appendChild(xSpan);
-    span.className ='close';
-    node.appendChild(span);
-     var close = document.getElementsByTagName('SPAN');
-       
-
+// code for list items to delete when clicked "x" button
+      var close = document.getElementsByTagName('SPAN');    
     for (var i =1; i <= close.length-1; i++) { 
-    close[i].onclick = function() {
+        close[i].onclick = function() {  
+            if (this.className=='close') {
+                this.style.display = 'none';
+                this.parentElement.style.display = 'none';
+            }
+        }; 
+    } 
+
+
+// code for adding new list itme when clicked "Add" button
+var addButton= document.getElementsByClassName('addBtn');
+    for (var j=0; j<= addButton.length-1; j++) { 
+        addButton[j].addEventListener('click', function() {
+            var txt1 = this.previousSibling.previousSibling.value;
+            var node1 = document.createElement("LI");
+            var txtNode1 = document.createTextNode(txt1);
+            node1.appendChild(txtNode1);
+            this.parentElement.parentElement.appendChild(node1);
+            this.previousSibling.previousSibling.value =''; 
+
+            var span = document.createElement("span");
+            var xSpan = document.createTextNode("x");
+            span.appendChild(xSpan);
+            span.className ='close';
+            node1.appendChild(span);
+
+                for (var i =1; i <= close.length-1; i++) { 
+                    close[i].onclick = function() {
  
-     if (this.className=='close') {
-        this.style.display = 'none';
-        this.parentElement.style.display = 'none';
-     }
-    
-    }; 
+                        if (this.className=='close') {
+                        this.style.display = 'none';
+                         this.parentElement.style.display = 'none';
+                        }
+                    }; 
+                } 
+        });
+    }
+
+
 } 
-  });
-
-
-
-
-     var close = document.getElementsByTagName('SPAN');
-        //console.log(close);
-    for (var i =1; i <= close.length-1; i++) { 
-    close[i].onclick = function() {
-if (this.className=='close') {
-        this.style.display = 'none';
-        this.parentElement.style.display = 'none';}
-
-    }; 
-} 
-
-
-var txt1 = "Aman";
-var node1 = document.createElement("LI");
-    var txtNode1 = document.createTextNode(txt1);
-    node1.appendChild(txtNode1);
-document.getElementById("myUL").appendChild(node1);
-
-}
